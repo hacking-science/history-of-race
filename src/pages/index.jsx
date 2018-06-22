0c4ffbfa1a7437181257969bd30557b0e5cee0c7
crabdul <23074754+crabdul@users.noreply.github.com>
2018-06-22 22:52:49 +0100

    Remove redundant React components and gatsby configurations

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import Helmet from "react-helmet";
-import PostListing from "../components/PostListing/PostListing";
+import { Link } from "react-scroll";
+import SEO from "../components/SEO/SEO";
 import config from "../../data/SiteConfig";
 import Drawer from "../layouts/Drawer/Drawer";
 import Navigation from "../components/Navigation/Navigation";
 import SiteWrapper from "../layouts/SiteWrapper/SiteWrapper";
+import Footer from "../components/Footer/Footer";
 import MainHeader from "../layouts/MainHeader/MainHeader";
 import MainNav from "../layouts/MainNav/MainNav";
 import BlogLogo from "../components/BlogLogo/BlogLogo";
 import MenuButton from "../components/MenuButton/MenuButton";
 import PageTitle from "../components/PageTitle/PageTitle";
 import PageDescription from "../components/PageDescription/PageDescription";
-import Footer from "../components/Footer/Footer";
-import PaginatedContent from "../layouts/PaginatedContent/PaginatedContent";
+import SocialMediaIcons from "../components/SocialMediaIcons/SocialMediaIcons";
 
-class TagTemplate extends React.Component {
+class IndexTemplate extends React.Component {
   state = {
     menuOpen: false
   };
@@ -42,29 +43,20 @@ class TagTemplate extends React.Component {
   };
 
   render() {
-    const {
-      tag,
-      nodes,
-      page,
-      pages,
-      total,
-      limit,
-      prev,
-      next
-    } = this.props.pathContext;
-    const authorsEdges = this.props.data.authors.edges;
+
     return (
-      <Drawer isOpen={this.state.menuOpen}>
-        <Helmet title={`Posts tagged as "${tag}" | ${config.siteTitle}`} />
+      <Drawer className="home-template" isOpen={this.state.menuOpen}>
+        <Helmet title={config.siteTitle} />
 
         {/* The blog navigation links */}
         <Navigation config={config} onClose={this.handleOnClose} />
+
         <SiteWrapper>
           {/* All the main content gets inserted here */}
-          <div className="tag-template">
+          <div className="home-template">
             {/* The big featured header */}
-            <MainHeader className="tag-head" cover={tag.featureImage}>
-              <MainNav>
+            <MainHeader cover={config.siteCover}>
+              <MainNav overlay={config.siteCover}>
                 <BlogLogo logo={config.siteLogo} title={config.siteTitle} />
                 <MenuButton
                   navigation={config.siteNavigation}
@@ -73,26 +65,27 @@ class TagTemplate extends React.Component {
               </MainNav>
               <div className="vertical">
                 <div className="main-header-content inner">
-                  <PageTitle text={tag} />
-                  <PageDescription
-                    text={tag.description || `A ${total}-post collection`}
+                  <PageTitle text={config.siteTitle} />
+                  <PageDescription text={config.siteDescription} />
+                  <SocialMediaIcons
+                    urls={config.siteSocialUrls}
+                    color="currentColor"
                   />
                 </div>
               </div>
+              <Link
+                className="scroll-down icon-arrow-left"
+                to="content"
+                data-offset="-45"
+                spy
+                smooth
+                duration={500}
+              >
+                <span className="hidden">Scroll Down</span>
+              </Link>
             </MainHeader>
-
-            <PaginatedContent
-              page={page}
-              pages={pages}
-              total={total}
-              limit={limit}
-              prev={prev}
-              next={next}
-            >
-              {/* PostListing component renders all the posts */}
-              <PostListing postEdges={nodes} postAuthors={authorsEdges} />
-            </PaginatedContent>
           </div>
+
           {/* The tiny footer at the very bottom */}
           <Footer
             copyright={config.copyright}
@@ -104,44 +97,4 @@ class TagTemplate extends React.Component {
   }
 }
 
-/* eslint no-undef: "off" */
-export const pageQuery = graphql`
-  query TagPage($tag: String) {
-    allMarkdownRemark(
-      limit: 1000
-      sort: { fields: [frontmatter___date], order: DESC }
-      filter: { frontmatter: { tags: { in: [$tag] } } }
-    ) {
-      totalCount
-      edges {
-        node {
-          fields {
-            slug
-          }
-          excerpt
-          timeToRead
-          frontmatter {
-            title
-            tags
-            cover
-            date
-          }
-        }
-      }
-    }
-    # authors
-    authors: allAuthorsJson {
-      edges {
-        node {
-          id
-          name
-          image
-          url
-          bio
-        }
-      }
-    }
-  }
-`;
-
-export default TagTemplate;
+export default IndexTemplate;
