acac9bc88f23c6f1f05df74e3abda2ec42d3163a
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2018-09-13 15:16:19 +0200

    Added YouTube playlist of recorded lectures

--- src/layouts/MainContent/MainContent.jsx
+++ src/layouts/MainContent/MainContent.jsx
@@ -24,25 +24,18 @@ class MainContent extends Component {
              <p>This event will be targeted at young
                  people, educators and youth workers (as well as members of the public). Lunch will be provided for the young people attending.</p>
 
-                 <h3>Why are we hosting this event?</h3>
-          <p>Conversations around race and racism
-              often take our attention to the United States. This is despite the fact that there is a rich history of people of colour resisting state racism in Britain, from the 1976 Grunwick Strike -led by women of colour- to the organising of the British Black Panthers.</p>
-          <p>This initiative aims to bring
-              together experts and academics who will discuss the historic and often ignored ways in which people of colour have organised to combat racism, as well as how these struggles connect to present day movements.
-          </p>
-        <p>All the lectures will be recorded
-            and used to create a free online resource for anyone looking to learn or teach the history of Race, Racism and Resistance in Britain.</p>
-          <h3>Who can attend?</h3>
-        <p>The event is primarily aimed at sixth form students, teachers, educators and youth workers. If you are one of the above and the event is sold out please get in touch as we may have reserved spaces.</p>
-        <h3>How much does it cost?</h3>
-        <p>The event is free to attend.</p>
-             <h3>Event Details</h3>
-             <p>Location: <a href="https://www.google.co.uk/maps/place/SOAS+University+of+London/@51.5224686,-0.1313338,17z/data=!4m5!3m4!1s0x48761b31db1e0f19:0x14db538713f5b80b!8m2!3d51.52235!4d-0.129259">SOAS University</a></p>
-             <p>Register: <a href="https://www.eventbrite.co.uk/e/a-history-of-race-resistance-in-britain-tickets-46437967210?aff=eac2">Sign up</a></p>
-             <p>Time: Thursday 9th of August 10.30am-5pm & Friday 10th of August 10.30am-5pm </p>
-             <p>Email: info@raceinbritain.org</p>
+          <h3>What was the purpose behind the event?</h3>
+         <p>Conversations around race and racism
+             often take our attention to the United States. This is despite the fact that there is a rich history of people of colour resisting state racism in Britain, from the 1976 Grunwick Strike -led by women of colour- to the organising of the British Black Panthers.</p>
+         <p>This initiative aims to bring
+             together experts and academics who will discuss the historic and often ignored ways in which people of colour have organised to combat racism, as well as how these struggles connect to present day movements.
+         </p>
 
+         <h3>Recorded Lectures</h3>
 
+         All the lectures were recorded and are available here as a free online resource for anyone looking to learn or teach the history of Race, Racism and Resistance in Britain.
+
+         <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PL2s-Xl42qeRXyIXjS_RoNdxl0AWxA2Ez-" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 
       </main>
     );
@@ -50,3 +43,18 @@ class MainContent extends Component {
 }
 
 export default MainContent;
+
+/*
+ <!--
+<p>All the lectures will be recorded
+   and used to create a free online resource for anyone looking to learn or teach the history of Race, Racism and Resistance in Britain.</p>
+ <h3>Who can attend?</h3>
+<p>The event is primarily aimed at sixth form students, teachers, educators and youth workers. If you are one of the above and the event is sold out please get in touch as we may have reserved spaces.</p>
+<h3>How much does it cost?</h3>
+<p>The event is free to attend.</p>
+    <h3>Event Details</h3>
+    <p>Location: <a href="https://www.google.co.uk/maps/place/SOAS+University+of+London/@51.5224686,-0.1313338,17z/data=!4m5!3m4!1s0x48761b31db1e0f19:0x14db538713f5b80b!8m2!3d51.52235!4d-0.129259">SOAS University</a></p>
+    <p>Register: <a href="https://www.eventbrite.co.uk/e/a-history-of-race-resistance-in-britain-tickets-46437967210?aff=eac2">Sign up</a></p>
+    <p>Time: Thursday 9th of August 10.30am-5pm & Friday 10th of August 10.30am-5pm </p>
+    <p>Email: info@raceinbritain.org</p>
+     -->*/
